2d80c7798f6bf4f54569d2018726e217c8ac8d64
KyzukY <kyzuk.yr@gmail.com>
2022-05-04 14:50:05 +0300

    ts_hw_1

--- interfaces/starlink.interface.ts
+++ interfaces/starlink.interface.ts
@@ -1,30 +1,35 @@
-export interface IStarlink {
-    mission_name: string;
-    launch_date_local: string;
+interface IPayload {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
+
+interface ICore {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string
+    }
+}
+
+interface IStarlink {
+    mission_name: string
+    launch_date_local: string
     launch_site: {
         site_name_long: string
     },
     links: {
-        article_link: null;
-        video_link: string;
+        article_link: string,
+        video_link: string
     },
     rocket: {
-        rocket_name: string;
+        rocket_name: string
         first_stage: {
-            cores: [
-                flight: number,
-                core: {
-                    reuse_count: number,
-                    status: string
-                }
-            ]
+            cores: ICore[]
+        },
+        second_stage: {
+            payloads: IPayload[]
         }
-    },
-    second_stage: {
-        payloads: [
-            payload_type: string,
-            payload_mass_kg: number,
-            payload_mass_lbs: number
-        ]
     }
 }
